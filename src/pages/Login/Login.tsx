2e0d38da2793bf1203d9bb28f623e4117b98c97b
Fabio Ginja Domingues <fabio.domingues@codeworks.fr>
2024-05-28 17:28:31 +0200

    feat: use of useCounter

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,10 +1,16 @@
+import { useCounter } from '../../hooks/useCounter';
+
 const Login = () => {
-    return (
-      <section>
-        <h1>Login</h1>
-      </section>
-    )
-  }
-  
-  export default Login
-  
+  const { counter, increment, decrement, reset } = useCounter(15);
+  return (
+    <section>
+      <h1>Login</h1>
+      <p>Counter: {counter}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+    </section>
+  );
+};
+
+export default Login;
