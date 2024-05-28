a156244a59f6410b6c215c56d24960902a592bd1
Fabio Ginja Domingues <fabio.domingues@codeworks.fr>
2024-05-28 16:33:13 +0200

    refactor: HeroCard with more info

--- src/pages/Heroes/Heroes.tsx
+++ src/pages/Heroes/Heroes.tsx
@@ -1,16 +1,51 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Hero } from '../../types/hero';
-import { HeroCard } from '../../components/HeroCard';
+import HeroCard from '../../components/HeroCard/HeroCard';
 import { getHeroesByLetter } from '../../api/hero';
 import arrayOfLetters from './utils';
+import Loader from '../../components/Loader/Loader';
 
 const Heroes = () => {
-  const [selectedLetter, setSelectedLetter] = useState('');
+  const [selectedLetter, setSelectedLetter] = useState('a');
   const [heroes, setHeroes] = useState<Hero[] | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    console.log('Création uniquement - []');
+    return () => {
+      console.log('Déstruction - []');
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log(
+      'Création ET mise à jour de la lettre séléctionnée - [selectedLetter: ${selectedLetter}]',
+    );
+    const load = async () => {
+      setLoading(true);
+      setHeroes(null);
+      try {
+        const data = await getHeroesByLetter(selectedLetter);
+        setHeroes(data);
+      } catch {
+        setError('Problem during fetching data')
+      } finally {
+        setLoading(false);
+      }
+    };
+    load();
+    return () => {
+      // AbortSignal of fetch request
+      console.log(`Déstruction - [selectedLetter: ${selectedLetter}]`);
+    };
+  }, [selectedLetter]);
+
+  // Création - Fetch les heros avec la lettre A
+  // Mise à jour - Fetch les heros avec la lettre B
+  // Destruction - Suppression de tout les abonnements
   const onSelectLetter = async (letter: string) => {
-      setSelectedLetter(letter);
-      const data = await getHeroesByLetter(letter);
-      setHeroes(data);
+    setSelectedLetter(letter);
   };
   return (
     <section>
@@ -26,7 +61,9 @@ const Heroes = () => {
           </li>
         ))}
       </ul>
-      <div>{heroes && heroes.map((hero) => <HeroCard key={hero.id} hero={hero} />)}</div>
+      {error && <p className='text-red-500'>{error}</p> }
+      {loading && <Loader />}
+      <div className='flex flex-wrap justify-center gap-4'>{heroes && heroes.map((hero) => <HeroCard key={hero.id} hero={hero} />)}</div>
     </section>
   );
 };
